4159e29ecd635f55dd09656c31a1a32e3a47ab15
GSinnott <g@sinnott.cc>
2021-03-16 04:12:50 +0100

    loads

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -21,27 +21,54 @@
 	});
 
 
-	function ui( value, name, min, max, step ) {
+	function ui( value, name, unit, min, max, step  ) {
 		return {
 			value,
 			name,
 			min,
 			max,
-			step
+			step,
+			unit
 		}
 	}
+	const MM = 'mm'
+	const NUM = 'num'
+	const DEG = 'deg'
+	const TARGET = 'target'
+
+	const targets = {
+		top: 'top',
+		left: 'left',
+		front: 'front',
+		back: 'back',
+		right: 'right',
+		bottom: 'bottom'
+	}
+
 
 	let conf = {
-		width: ui( 200, 'box width'),
-		height: ui( 251, 'box height'),
-		depth: ui( 400, 'box depth'),
-		thickness: ui( 10, 'material thickness', 0, 10),
-		tabs: ui( 2, 'tabs amount'),
-		offset: ui( 0, 'tabs offset', 0, 1, 0.1),
-		overhang: ui( 20, 'case overhang'),
-		slot: ui( 0, 'slot amount' ),
-		size: ui( 10, 'slot size' )
+		width: ui( 120, 'box width', MM),
+		height: ui( 120, 'box height', MM),
+		depth: ui( 120, 'box depth', MM),
+		thickness: ui( 15, 'material thickness', MM, 0, 20),
+		tabs: ui( 5, 'side tabs count', NUM),
+		offset: ui( 0.5, 'side tabs offset', NUM, 0, 1, 0.1),
+		overhang: ui( 5, 'panel overhang', MM, 2),
+		slot: ui( 0, 'panel slots', NUM ),
+		size: ui( 10, 'panel slot size', MM )
 	}
+
+	const HOLES = {
+		diameter: ui( 5, 'hole diameter', MM ),
+		offset: ui( 20, 'hole offset', MM, 0),
+		count: ui( 4, 'hole amount', NUM, 1 ),
+		rotate: ui( 0, 'rotation', DEG )
+	}
+
+	let els = [
+
+	]
+
 
 
 	function svg( conf ) {
@@ -77,29 +104,87 @@
 		const size = conf.size.value
 		const slot = conf.slot.value
 		const offset = parseFloat( conf.offset.value ) || 0
+		const overhang = conf.overhang.value
 
-		const w = conf.width.value + t
+		let w = conf.width.value
 		const h = conf.height.value
 		let x = 0
 		const y = h
+		const middle = h + (overhang * 2) + (t * 2)
 
 		let faces = {}
 
-		// const ff = ['top', 'bottom']
-		// ff.forEach( (f, i) => {
-		// 	const b = i%2 == 0
-		// 	let xx = h + t
-		// 	let yy = (b) ? (h * 1): h * 2
-		// 	yy += t * 2
-		// 	if (b) yy *= -1
-		// 	faces[f] = { 
-		// 		models: {
-		// 			rect: new maker.rectangle(w,h) 
-		// 		},
-		// 		origin: [xx, yy]
-		// 	}
+		const ff = ['top', 'bottom']
+		ff.forEach( (f, i) => {
+			let xx = h + t
+			const mod = i%2 == 0
+			const oo = overhang * 2
+			const ww = w + oo
+			const hh = h + oo
+			let face = {
+				models: {
+					rect: new maker.rectangle(ww,hh) 
+				},
+				origin: [xx - overhang, mod ? 0 : middle + d + (t * 2)]
+			}
 
-		// })
+			let rot = i%2 == 0
+
+			
+			const www = size + t
+			let corner = {
+				models: {
+
+					a: maker.move( 
+						new maker.rectangle( www, t ),
+						[overhang, overhang]
+					),
+					b: maker.move( 
+						new maker.rectangle( t, www ),
+						[overhang, overhang]
+					)
+				}
+			}
+			maker.combine( corner.models.a, corner.models.b )
+
+			let four = [
+				{
+					name: 'bl',
+					rot: 0,
+					xy: [0,0]
+				},
+				{
+					name: 'br',
+					rot: 90,
+					xy: [ww,0]
+				},
+				{
+					name: 'tr',
+					rot: 180,
+					xy: [ww,hh]
+				},
+				{
+					name: 'tl',
+					rot: 270,
+					xy: [0,hh]
+				}
+			]
+
+			four.forEach( u => {
+				const c = JSON.parse( JSON.stringify( corner ) )
+
+				maker.move( c, u.xy )
+				makerjs.model.rotate( c, u.rot, u.xy )
+				face.models[u.name] = c
+
+			})
+
+
+			faces[f] = face
+
+		})
+
+		w = conf.width.value + t
 
 		ss.forEach( (s, i) => {
 
@@ -201,8 +286,7 @@
 				if (first && even && !full) cc.x += t
 				if (!first && even && !zero) cc.x += t
 
-
-				// console.log(cc.x + cc.w, ww + x)
+				if (!odd) cc.x -= size - t
 
 				const ba = cc.x + cc.w == ww + x
 				const bb = cc.x == x
@@ -223,6 +307,7 @@
 				m.models[ k ] = r
 				const op = 'union'
 
+
 				maker.combine( m.models.rect, m.models[ k ],false, true, false, true )
 			})
 
@@ -235,9 +320,28 @@
 		}) 
 
 
+		els.forEach( el => {
+			const tar = el.target.value
+			const all = { ...sides, ...faces }
+			const rr = all[tar]
+			const bb = makerjs.measure.modelExtents( rr )
+			console.log('>>>>', tar, rr, bb)
+		})
+
+		console.log( '?', sides, faces )
 		
 
-		return maker.svg( { models: { ...sides, ...faces } } )
+		return maker.svg( { 
+			models: { 
+				sides: {
+					models: sides,
+					origin: [0,middle]
+				}, 
+				faces: {
+					models: faces
+				}
+			} 
+		})
 
 	}
 
@@ -250,15 +354,33 @@
 		width: ( conf.depth * 2 ) + conf.width,
 		height: ( conf.height * 2 ) + ( conf.depth * 2)
 	}
+
+	let sidebar = 'minw220px h100vh overflow-auto flex column-stretch-flex-start p1'
+
+
+	function addElement( type ) {
+		const arr = els
+		els = []
+		arr.push({
+			target: ui( targets.top, 'target', TARGET),
+			...type
+		})
+		els = arr
+	}
+
+	addElement( HOLES )
 </script>
 
 <main>
 
 	<div class="flex row-stretch-stretch">
-		<aside class="minw220px h100vh overflow-auto flex column-stretch-flex-start p1">
+		<aside class={sidebar}>
 			{#each Object.entries(conf) as [key, value]}
 				<fieldset >
-					<div class="mtb1">{key}</div>
+					<div class="mtb1">
+						{value.name}
+						<span class="fade">({value.unit})</span>
+					</div>
 
 					<input 
 						on:keyup={onKeyUp} 
@@ -273,10 +395,56 @@
 			{/each}
 		</aside>
 
-		<section class="p1 grow">
+		<section class="p1 grow flex column-center-center">
 			{@html makerjs ? svg( conf ) : '' }
 
 		</section>
+
+		<aside class={sidebar}>
+<!-- 
+	const MM = 'mm'
+	const NUM = 'num'
+	const DEG = 'deg'
+	const TARGET = 'target' -->
+			{#each els as el}
+				{#each Object.entries(el) as [key, value]}
+					<fieldset >
+						<div class="mtb1">
+							{value.name}
+							<span class="fade">({value.unit})</span>
+						</div>
+
+						{#if value.unit == TARGET }
+							<select>
+								{#each Object.entries(targets) as [k, v]}
+									<option value={k} name={k}>{v}</option>
+								{/each}
+							</select>
+						{:else}
+
+							<input 
+								on:keyup={onKeyUp} 
+								type="number" 
+								min={value.min}
+								max={value.max}
+								step={value.step || 1}
+								bind:value={el[key].value} 
+								placeholder="width">
+
+						{/if}
+
+					</fieldset>
+
+				{/each}
+
+			{/each}
+			<div class="cmt1">
+				<button on:click={e => addElement( HOLES )}>
+					add holes
+				</button>
+			</div>
+		</aside>
+
 
 	</div>
 </main>
